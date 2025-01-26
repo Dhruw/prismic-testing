2de242871cd9694267dd51f94d86ec1cda0022fd
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-01-26 17:40:40 +0530

    Style header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@ import { createClient } from '@prismicio/client';
 import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
 import config from '../../slicemachine.config.json';
+import Logo from '@/components/Logo';
+import Bounded from './Bounded';
 
 async function Footer() {
   const client = createClient(config.repositoryName);
@@ -9,19 +11,23 @@ async function Footer() {
   const settings = await client.getSingle('settings');
 
   return (
-    <footer>
-      <Link href="/">{settings.data.site_title}</Link>
-      <div>
-        &copy; {new Date().getFullYear()} {settings.data.site_title}
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} {settings.data.site_title}
+        </p>
+        <ul className="flex">
+          {settings.data.navigation.map(({ label, link }) => (
+            <li key={label}>
+              <PrismicNextLink field={link} />
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {settings.data.navigation.map(({ label, link }) => (
-          <li key={label}>
-            <PrismicNextLink field={link} />
-          </li>
-        ))}
-      </ul>
-    </footer>
+    </Bounded>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import { createClient } from '@prismicio/client';
 import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
 import config from '../../slicemachine.config.json';
+import Bounded from '@/components/Bounded';
+import Logo from '@/components/Logo';
 
 async function Header() {
   const client = createClient(config.repositoryName);
@@ -9,18 +11,22 @@ async function Header() {
   const settings = await client.getSingle('settings');
 
   return (
-    <header>
-      <Link href="/">{settings.data.site_title}</Link>
-      <nav>
-        <ul>
-          {settings.data.navigation.map(({ label, link }) => (
-            <li key={label}>
-              <PrismicNextLink field={link} />
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <Bounded as="header" className="py-4 md:py-6 lg:py-8">
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav>
+          <ul className="flex">
+            {settings.data.navigation.map(({ label, link }) => (
+              <li key={label}>
+                <PrismicNextLink field={link} className="p-3" />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Bounded>
   );
 }
 
