7dcea25146a1b789fdf72921d740eb275260397d
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-01-27 11:03:41 +0530

    Testimonial Slice styling

--- src/slices/Testimonials/index.tsx
+++ src/slices/Testimonials/index.tsx
@@ -1,5 +1,5 @@
-import { Content } from '@prismicio/client';
-import { PrismicNextLink } from '@prismicio/next';
+import { Client, Content, createClient, isFilled } from '@prismicio/client';
+import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import {
   JSXMapSerializer,
   PrismicRichText,
@@ -8,6 +8,7 @@ import {
 import { JSX } from 'react';
 import Bounded from '@/components/Bounded';
 import Heading from '@/components/Heading';
+import config from '../../../slicemachine.config.json';
 
 const components: JSXMapSerializer = {
   heading2: ({ children }) => (
@@ -25,7 +26,7 @@ const components: JSXMapSerializer = {
     </Heading>
   ),
   paragraph: ({ children }) => (
-    <p className="font-medium font-body text-slate-600 sm:text-left text-center">
+    <p className="text-xl md:text-2xl font-normal font-body text-slate-600 mb-8 ">
       {children}
     </p>
   ),
@@ -39,16 +40,63 @@ export type TestimonialsProps = SliceComponentProps<Content.TestimonialsSlice>;
 /**
  * Component for "Testimonials" Slices.
  */
-const Testimonials = ({ slice }: TestimonialsProps): JSX.Element => {
+const Testimonials = async ({
+  slice,
+}: TestimonialsProps): Promise<JSX.Element> => {
+  const client = createClient(config.repositoryName);
+
+  const testimonials = await Promise.all(
+    slice.primary.testimonial.map((testimonial) => {
+      if (
+        isFilled.contentRelationship(testimonial.testimonial) &&
+        testimonial.testimonial.uid
+      ) {
+        return client.getByUID('testimonial', testimonial.testimonial.uid);
+      }
+    })
+  );
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
       <PrismicRichText field={slice.primary.heading} components={components} />
-      {slice.primary.testimonial.map((item, index) => (
-        <div key={index}>{console.log(item)} Item</div>
-      ))}
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        {testimonials.map((testimonial) => {
+          return (
+            <div
+              key={testimonial?.uid}
+              className="border bg-white shadow-lg  rounded-lg px-8 md:px-14 py-10 md:py-16 grid content-between"
+            >
+              <PrismicRichText
+                field={testimonial?.data.quote}
+                components={components}
+              />
+              <div className="flex items-center">
+                <div>
+                  <PrismicNextImage
+                    field={testimonial?.data.avatar}
+                    height={56}
+                    width={56}
+                    className="rounded-full mr-4"
+                    imgixParams={{ ar: '1:1', fit: 'crop' }}
+                  />
+                </div>
+                <div>
+                  <p className="text-base font-medium text-slate-700">
+                    {testimonial?.data.name}
+                  </p>
+                  <p className="text-base text-slate-600">
+                    {testimonial?.data.job_title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </Bounded>
   );
 };
