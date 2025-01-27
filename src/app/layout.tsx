1024a8892c64a7dbc1bec551407bac69b62e0116
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-01-27 13:24:32 +0530

    Added green backgrodun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
         {children}
         <PrismicPreview repositoryName={repositoryName} />
         <Footer />
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50"></div>
       </body>
     </html>
   );
