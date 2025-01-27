e78076fa2c7f7040fd64b9483e3ec9882152c560
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-01-27 10:33:13 +0530

    Before adding query for individual Testimonial

--- src/slices/Testimonials/index.tsx
+++ src/slices/Testimonials/index.tsx
@@ -1,5 +1,35 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content } from '@prismicio/client';
+import { PrismicNextLink } from '@prismicio/next';
+import {
+  JSXMapSerializer,
+  PrismicRichText,
+  SliceComponentProps,
+} from '@prismicio/react';
+import { JSX } from 'react';
+import Bounded from '@/components/Bounded';
+import Heading from '@/components/Heading';
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="md" className="text-center mb-12">
+      {children}
+    </Heading>
+  ),
+  heading3: ({ children }) => (
+    <Heading
+      as="h3"
+      size="sm"
+      className="mb-3 font-medium sm:text-left text-center"
+    >
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="font-medium font-body text-slate-600 sm:text-left text-center">
+      {children}
+    </p>
+  ),
+};
 
 /**
  * Props for `Testimonials`.
@@ -11,13 +41,15 @@ export type TestimonialsProps = SliceComponentProps<Content.TestimonialsSlice>;
  */
 const Testimonials = ({ slice }: TestimonialsProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for testimonials (variation: {slice.variation})
-      Slices
-    </section>
+      <PrismicRichText field={slice.primary.heading} components={components} />
+      {slice.primary.testimonial.map((item, index) => (
+        <div key={index}>{console.log(item)} Item</div>
+      ))}
+    </Bounded>
   );
 };
 
