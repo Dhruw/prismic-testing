06b284387f46067410ec17f5b3081ef8d9dcb2cc
Divyanshu Dhruw <divyanshudhruw@gmail.com>
2025-01-27 12:39:16 +0530

    Image and Text Slice styling

--- src/slices/TextWithImage/index.tsx
+++ src/slices/TextWithImage/index.tsx
@@ -1,5 +1,26 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content } from '@prismicio/client';
+import {
+  JSXMapSerializer,
+  PrismicRichText,
+  SliceComponentProps,
+} from '@prismicio/react';
+import Bounded from '@/components/Bounded';
+import Heading from '@/components/Heading';
+import Button from '@/components/Button';
+import { PrismicNextImage } from '@prismicio/next';
+import { JSX } from 'react';
+import clsx from 'clsx';
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="lg" className="">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="max-w-md text-lg font-body text-slate-600">{children}</p>
+  ),
+};
 
 /**
  * Props for `TextWithImage`.
@@ -12,13 +33,31 @@ export type TextWithImageProps =
  */
 const TextWithImage = ({ slice }: TextWithImageProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for text_with_image (variation: {slice.variation})
-      Slices
-    </section>
+      <div className="grid gap-8 md:grid-cols-2 place-items-center">
+        <PrismicNextImage
+          field={slice.primary.image}
+          //@ts-ignore
+          alt={slice?.primary?.image?.alt || ''}
+          //@ts-ignore
+          fallbackAlt="Image"
+          className={clsx(
+            'rounded-lg',
+            slice.variation === 'imageRight' && 'md:order-2'
+          )}
+        />
+        <div className="grid gap-4">
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={components}
+          />
+          <PrismicRichText field={slice.primary.body} components={components} />
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
